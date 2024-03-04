5b82a015a09567a9a1bd984fe61cb07c4df70e51
KoshinHagimoto <koshin.hagimoto@gmail.com>
2024-03-04 22:41:13 +0900

    added UserDAO

--- app/dao/users.go
+++ app/dao/users.go
@@ -1,6 +1,10 @@
 package dao
 
-import "database/sql"
+import (
+	"database/sql"
+	"fmt"
+	"text-snippet/app/object"
+)
 
 type UserDAO struct {
 	db *sql.DB
@@ -8,4 +12,28 @@ type UserDAO struct {
 
 func NewUserDAO(db *sql.DB) *UserDAO {
 	return &UserDAO{db: db}
+}
+
+func (dao *UserDAO) CreateUser(user *object.User) error {
+	query := `INSERT INTO users (username, email, password_hash) VALUES (?, ?, ?)`
+	_, err := dao.db.Exec(query, user.Username, user.Email, user.PasswordHash)
+	if err != nil {
+		return fmt.Errorf("error creating user: %v", err)
+	}
+	return nil
+}
+
+func (dao *UserDAO) GetUserByID(id int) (*object.User, error) {
+	query := `SELECT id, username, email, password_hash, created_at, email_verified FROM users WHERE id = ?`
+	row := dao.db.QueryRow(query, id)
+
+	var user object.User
+	err := row.Scan(&user.ID, &user.Username, &user.Email, &user.PasswordHash, &user.CreatedAt, &user.EmailVerified)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return nil, fmt.Errorf("user not found: %v", err)
+		}
+		return nil, fmt.Errorf("error getting user: %v", err)
+	}
+	return &user, nil
 }
